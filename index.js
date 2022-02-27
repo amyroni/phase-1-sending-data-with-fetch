0e01ce4af4b3607a6bc2335fa41ad400abde7ad6
Amy Shi <amyhshi92@gmail.com>
2022-02-26 19:34:48 -0500

    commit tree da0313b54b4d405216019a9c454a709a8438450e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(username, useremail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: username,
+      email: useremail,
+    })
+  })
+  .then(response => response.json())
+  .then(userData => {
+    document.body.innerHTML = userData.id
+    // const user = document.createElement("p")
+    // user.innerText = userData.id
+    // document.querySelector("body").appendChild(user)
+  })
+  .catch((error) => {
+    document.body.innerHTML = error.message
+    // const errorMessage = document.createElement("p")
+    // errorMessage.innerText = error
+    // document.querySelector("body").appendChild(errorMessage)
+  })
+}
+
+
